4333d9b2469e87f4c9ca63954aacbbdf529d7283
Frank Zhao <frank@frankzhao.net>
2014-05-01 21:35:59 +1000

    added apt-get make for vagrant

--- install_clibs.sh
+++ install_clibs.sh
@@ -3,7 +3,7 @@
 # frank.zhao@anu.edu.au
 
 cd $HOME
-sudo apt-get install g++ subversion cmake
+sudo apt-get install g++ subversion cmake make
 svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
 mkdir build_libcxx && cd build_libcxx
 CC=clang CXX=clang++ cmake -G "Unix Makefiles" -DLIBCXX_CXX_ABI=libsupc++ -DLIBCXX_LIBSUPCXX_INCLUDE_PATHS="/usr/include/c++/4.6/;/usr/include/c++/4.6/x86_64-linux-gnu/" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr $HOME/libcxx
